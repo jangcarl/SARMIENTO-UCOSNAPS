a02fe3a623ecab29b42c14c6955373c056b0e792
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-12-11 20:05:26 +0800

    Update dbConfig.php

--- core/dbConfig.php
+++ core/dbConfig.php
@@ -3,10 +3,11 @@ session_start();
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "search_user_return_response";
+$dbname = "album";
 $dsn = "mysql:host={$host};dbname={$dbname}";
 
 $pdo = new PDO($dsn,$user,$password);
 $pdo->exec("SET time_zone = '+08:00';");
+date_default_timezone_set('Asia/Manila');
 
 ?>
